e33f41852731e40c74d0d20e1b87d7a675f151cb
Boco0711 <s.lep.0711@gmail.com>
2019-09-11 12:03:26 +0200

    Lancement du jeu dans le Main

--- src/com/ocr/sylvain/Main.java
+++ src/com/ocr/sylvain/Main.java
@@ -3,6 +3,7 @@ package com.ocr.sylvain;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Game game = new Game();
+        game.Combat();
     }
 }
