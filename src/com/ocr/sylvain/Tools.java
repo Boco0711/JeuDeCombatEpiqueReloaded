ea3bbb94cca7529fe92da7bd5199d86705f7e8e2
Boco0711 <s.lep.0711@gmail.com>
2019-09-11 09:53:31 +0200

    Création de la méthode AskSomething et mise au vert des tests

--- src/com/ocr/sylvain/Tools.java
+++ src/com/ocr/sylvain/Tools.java
@@ -1,9 +1,32 @@
 package com.ocr.sylvain;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Tools {
 
-    private Scanner sc;
+
+    public static int askSomething(String question, String error, int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(question);
+        boolean responseIsGood = false;
+        int choix = 0;
+        while (!responseIsGood || choix < min || choix > max) {
+            try {
+                choix = sc.nextInt();
+                responseIsGood = true;
+                if (choix < min || choix > max) {
+                    System.out.println(error);
+                    System.out.println(question);
+                }
+            } catch (InputMismatchException e) {
+                sc.next();
+                System.out.println(error);
+                System.out.println(question);
+                responseIsGood = false;
+            }
+        }
+        return choix;
+    }
 
 }
