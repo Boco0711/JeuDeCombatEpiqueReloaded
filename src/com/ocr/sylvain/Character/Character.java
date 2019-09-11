ec0ca6aa9adc209eda311936f3c9aecf7392b5ea
Boco0711 <s.lep.0711@gmail.com>
2019-09-11 10:12:44 +0200

    Méthode askStats ajouté dans Character pour l'initialisation des statistiques

--- src/com/ocr/sylvain/Character/Character.java
+++ src/com/ocr/sylvain/Character/Character.java
@@ -1,15 +1,27 @@
 package com.ocr.sylvain.Character;
 
+import com.ocr.sylvain.Tools;
+
 public class Character {
     private String name;
     private int level;
     private int vitality;
     private int strenght;
     private int agility;
-    private int inttelligence;
+    private int intelligence;
 
     public Character(String name){
         this.name = name;
+        this.level = askStats("Niveau", 1, 100);
+        this.vitality = this.level*5;
+        this.strenght = askStats("Force", 0, this.level);
+        this.agility = askStats("Agilité", 0, (this.level-this.strenght));
+        this.intelligence = askStats("Intelligence", 0, (this.level - (this.strenght+this.agility)));
+    }
+
+    private int askStats(String toAsk, int min, int max) {
+        String error = toAsk + " doit être compris entre " + min + " et " + max;
+        return Tools.askSomething(error, toAsk, min, max);
     }
 
     public String getName() {
@@ -32,7 +44,7 @@ public class Character {
         return this.agility;
     }
 
-    public int getInttelligence() {
-        return this.inttelligence;
+    public int getIntelligence() {
+        return this.intelligence;
     }
 }
